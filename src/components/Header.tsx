473611014d1fa780fe5c62c7d299111d5b3dc954
stokes1003 <stokes1003@gmail.com>
2025-03-23 13:15:41 -0500

    mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Burger, Container, Group, Text, Box } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import classes from "../HeaderMenu.module.css";
@@ -21,6 +21,7 @@ export function HeaderMenu({ setIsLeaderboard }) {
         if (link === "Add Scores") {
           setIsLeaderboard(false);
         }
+        toggle(); // Close menu after clicking
       }}
     >
       {link}
@@ -39,11 +40,22 @@ export function HeaderMenu({ setIsLeaderboard }) {
               <Text fw={800}>Fairway Fleas</Text>
             </Box>
 
+            {/* Desktop Menu */}
             <Group gap={5} visibleFrom="sm">
               {items}
             </Group>
+
+            {/* Mobile Burger Menu */}
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              size="sm"
+              hiddenFrom="sm"
+            />
           </Group>
-          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+
+          {/* Mobile Menu */}
+          {opened && <Box className={classes.mobileMenu}>{items}</Box>}
         </Box>
       </Container>
     </header>
